96324bd23e1220d63acfcc275bee381a6c9bb841
ColorII <beefalo306@gmail.com>
2024-05-18 12:11:19 +0300

    Lesson_2

---
+++ src/main/java/lv/javaguru/java1/student_dmitry_voronin/lesson_1/lesson_2/level_1/level_7/NoSleepNoFocus.java
@@ -0,0 +1,8 @@
+package lv.javaguru.java1.student_dmitry_voronin.lesson_1.lesson_2.level_1.level_7;
+
+class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+}
